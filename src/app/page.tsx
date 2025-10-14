e7ce348ea1c05b5f8f8da14e46808e74d1b34b39
Sebastián Berrios Gómez <sebberrios99@gmail.com>
2025-10-14 15:26:06 -0500

    feat: configure Supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
+export default async function Page() {
+  const cookieStore = cookies();
+  const supabase = await createClient(cookieStore);
+
   return <></>;
 }
